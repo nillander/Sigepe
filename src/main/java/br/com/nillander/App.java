1f25ee419ab609455a161d1e1243b86f28cccfca
Nillander Alarcão <nillander@live.com>
2024-10-07 23:06:25 -0300

    Primeiro commit do projeto Sigepe

---
+++ src/main/java/br/com/nillander/App.java
@@ -0,0 +1,9 @@
+package br.com.nillander;
+
+public class App {
+
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+    }
+}
---
+++ src/main/java/br/com/nillander/UserRepository.java
@@ -0,0 +1,24 @@
+package br.com.nillander;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class UserRepository {
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("MyPU");
+
+    public void saveUser(User user) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(user);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public User findUser(Long id) {
+        EntityManager em = emf.createEntityManager();
+        User user = em.find(User.class, id);
+        em.close();
+        return user;
+    }
+}
